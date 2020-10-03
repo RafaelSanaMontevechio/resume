c2ee2dbbed8ba1ac448b451b6e6ad1b81287e9a7
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2020-10-03 15:34:31 -0300

    fix font

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import App from 'next/app'
-import '../css/styles.css'
+import React from 'react';
+import App from 'next/app';
+import '../css/styles.css';
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       <React.Fragment>
-        <style jsx global>
+        {/* <style jsx global>
         {`
             @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap');
-        `}
-        </style>
+    `}
+        </style>*/}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
         <Component {...pageProps} />
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default MyApp
+export default MyApp;
